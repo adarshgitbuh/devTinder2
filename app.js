05ff0735ed2459a03cc19240b30f6af31a8d14e9
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-03-23 10:39:47 +0530

    Route handler

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res) => {
-    res.send("hello world");
+app.get("/user/:userId/:pass", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "adarsh", lastName: "adhuu" });
+});
+
+app.post("/tester", (req, res) => {
+  console.log("The data saved sucessfuly");
+  res.send("hello world");
+});
+
+app.delete("/user", (res, req) => {
+  res.send("Delete the api call");
 });
 
 app.listen(4000, () => {
-    console.log("serever is running sucessfully");
+  console.log("serever is running sucessfully");
 });
