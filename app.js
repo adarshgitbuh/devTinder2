b1c7b005cac3225fd8a9c957ff61cc0ba7b13368
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-03-30 10:57:24 +0530

    "multiple route handlers"

--- app.js
+++ app.js
@@ -2,19 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:pass", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "adarsh", lastName: "adhuu" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("REquest handler");
+    next();
+    res.send("req handler");
+    //console.log("Req not handle");
+  },
+  //route handlers
+  (req, res) => {
+    console.log("Req not handler 2");
+    res.send("req not handle 2");
+  }
+);
 
-app.post("/tester", (req, res) => {
-  console.log("The data saved sucessfuly");
-  res.send("hello world");
-});
+// app.get("/user/:userId/:pass", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firstName: "adarsh", lastName: "adhuu" });
+// });
 
-app.delete("/user", (res, req) => {
-  res.send("Delete the api call");
-});
+// app.post("/tester", (req, res) => {
+//   console.log("The data saved sucessfuly");
+//   res.send("hello world");
+// });
+
+// app.delete("/user", (res, req) => {
+//   res.send("Delete the api call");
+// });
 
 app.listen(4000, () => {
   console.log("serever is running sucessfully");
