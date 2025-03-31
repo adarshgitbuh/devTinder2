2880f7e250ad9eac9bf7d778b18a855479fedb52
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-03-31 15:25:19 +0530

    writing auth midllewares

--- app.js
+++ app.js
@@ -2,20 +2,37 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("REquest handler");
-    next();
-    res.send("req handler");
-    //console.log("Req not handle");
-  },
-  //route handlers
-  (req, res) => {
-    console.log("Req not handler 2");
-    res.send("req not handle 2");
-  }
-);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+//handle middlewares
+app.use("/admin", adminAuth, (req, res, next) => {
+  res.send("Hello admin");
+});
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("Hello user");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent...");
+});
+
+app.get("/admin/DeleteData", (req, res) => {
+  res.send("Data deleted...");
+});
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("REquest handler");
+//     next();
+//     //res.send("req handler");
+//     //console.log("Req not handle");
+//   });
+//   //route handlers
+//  app.get("/user2", (req, res) => {
+//   console.log("You can use");
+//   next();
+//  })
 
 // app.get("/user/:userId/:pass", (req, res) => {
 //   console.log(req.params);
---
+++ middlewares/auth.js
@@ -0,0 +1,26 @@
+const adminAuth = (req, res, next) => {
+  console.log("Admin auth to be checked");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  console.log("user auth to be checked");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  adminAuth,
+  userAuth,
+};
